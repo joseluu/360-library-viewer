b57fd9d370425128d8527015082c4a9f2ceb63db
Jose Luu <jose.luu@gmail.com>
2026-04-05 12:47:17 +0200

    Use scp instead of rsync in deploy script (Windows compat)

--- deploy/rsync-deploy.sh
+++ deploy/rsync-deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# Rsync photos and confidential config to googlevm
+# Push photos and confidential config to googlevm
 # Run from the viewer/ directory
 
 REMOTE="googlevm"
 REMOTE_DIR="/opt/viewer-360"
 
 echo "=== Syncing photo directories ==="
-rsync -avz --progress \
-    Photos-360-Lisa-Damien-4-Avril/ \
-    "$REMOTE:$REMOTE_DIR/Photos-360-Lisa-Damien-4-Avril/"
+scp -r Photos-360-Lisa-Damien-4-Avril "$REMOTE:$REMOTE_DIR/"
 
 echo "=== Syncing confidential user config ==="
-rsync -avz --progress \
-    users-config.js \
-    "$REMOTE:$REMOTE_DIR/users-config.js"
+scp users-config.js "$REMOTE:$REMOTE_DIR/users-config.js"
 
 echo "=== Done ==="
